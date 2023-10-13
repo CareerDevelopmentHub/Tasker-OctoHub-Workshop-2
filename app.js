d51ca7fb9fdb15e9573cfb7855d42bd5b0c8474a
SomyakantSwain <130567791+SomyakantSwain@users.noreply.github.com>
2023-10-13 09:42:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -130,3 +130,23 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.addEventListener("DOMContentLoaded", getTodos);
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    
+    skew: false,
+    ease : "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0,
+  });
+  Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+ 
+
+
+
+
+
+
